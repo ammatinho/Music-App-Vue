bd554d8fdfa5895edd7e0f00a2239ce7af1db352
ammatinho <anamargaridamatinho@gmail.com>
2020-02-05 21:12:50 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,117 @@
+<template>
+  <div id="app">
+    <header>
+      <h1>My Music</h1>  
+    </header>
+    <main>
+      <section class="player">
+        <h2 class="song-title">{{ current.title }} - <span>{{ current.artist }}</span></h2>
+        <div class="control">
+          <button class="prev" @click="prev">Prev</button>
+          <button class="play" v-if="!isPlaying" @click="play">Play</button>
+          <button class="pause" v-else @click="pause">Pause</button>
+          <button class="next" @click="next">Next</button>
+        </div>
+      </section>
+      <section class="playlist">
+        <h3>The Playlist</h3>
+        <button v-for="song in songs" :key="song.title" @click="play(song)" :class="(song.title == current.src) ? 'song playing' : 'song'">
+          {{ song.title }} - {{ song.artist }}
+        </button>
+      </section>
+    </main>   
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'app',
+  data () {
+    return {
+      current: {},
+      index: 0,
+      isPlaying: false,
+      songs: [
+        {
+          title: "Party Police",
+          artist: "Alvvays",
+          src: require('./assets/Alvvays-PartyPolice.mp3')
+        },
+        {
+          title: "King Park",
+          artist: "La Dispute",
+          src: require('./assets/LaDispute-KingPark.mp3')
+        },
+        {
+          title: "Harbor",
+          artist: "Touché Amoré",
+          src: require('./assets/TouchéAmoré-Harbor.mp3')
+        }
+      ],
+      player: new Audio()
+    }
+  },
+  methods: {
+    play(song) {
+      if (typeof song.src != "undefined") {
+        this.current = song;
+
+        this.player.src = this.current.src;
+      }
+
+      this.player.play();
+      this.isPlaying = true;
+    },
+    pause() {
+      this.player.pause();
+      this.isPlaying = false;
+    },
+    next() {
+      this.index++;
+      if (this.index > this.songs.length - 1) {
+        this.index = 0;
+      }
+
+      this.current = this.songs[this.index];
+      this.play(this.current);
+    },
+    prev() {
+      this.index--;
+      if (this.index < 0) {
+        this.index = this.songs.length - 1;
+      }
+
+      this.current = this.songs[this.index];
+      this.play(this.current);
+    }
+  },
+  created () {
+    this.current = this.songs[this.index];
+    this.player.src = this.current.src;
+    // this.player.play(); 
+  }
+}
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  font-family: sans-serif;
+}
+
+header {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 15px;
+  background-color: #212121;
+  color: #FFF;
+}
+
+</style>
